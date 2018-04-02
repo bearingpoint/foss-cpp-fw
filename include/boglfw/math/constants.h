c9a31dba7a7916aeb1d59e93cb3e5aa4df9e816c
Bogdan I <bog2k3@gmail.com>
2018-04-02 10:39:20 +0300

    +INF constant

--- include/boglfw/math/constants.h
+++ include/boglfw/math/constants.h
@@ -9,6 +9,7 @@
 #define MATH_CONSTANTS_H_
 
 static constexpr auto EPS = 1.e-20f;
+static constexpr auto INF = 1.e20f;
 static constexpr auto PI = 3.1415926535897932384626433832795f;
 static constexpr auto PI_INV = 0.31830988618f;
 static constexpr auto E = 2.71828182845904523536f;
