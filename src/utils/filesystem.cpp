bd7589f9a3b2c5214fdf76b0f1e3035fd54ad0c2
Bogdan I <bog2k3@gmail.com>
2018-07-19 22:48:32 +0300

    new filesystem and string manip funcitonality added

---
+++ src/utils/filesystem.cpp
@@ -0,0 +1,171 @@
+/*
+ * dir.cpp
+ *
+ *  Created on: Oct 15, 2015
+ *      Author: bog
+ */
+
+#include <boglfw/utils/filesystem.h>
+#include <boglfw/utils/log.h>
+#include <boglfw/utils/strManip.h>
+
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/sendfile.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <utime.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+#include <fstream>
+
+namespace filesystem {
+
+bool isDir(std::string const& dir) {
+    struct stat fileInfo;
+    stat(dir.c_str(), &fileInfo);
+    return S_ISDIR(fileInfo.st_mode);
+}
+
+bool pathExists(std::string const& path) {
+	struct stat fileInfo;
+	return stat(path.c_str(), &fileInfo) == 0;
+}
+
+bool touchFile(std::string const& path) {
+	if (!pathExists(path)) {
+		std::ofstream f(path);
+		return f.is_open();
+	}
+	return !utime(path.c_str(), nullptr);
+}
+
+bool mkDir(std::string const& path) {
+	LOGPREFIX("mkDir");
+	if (mkdir(path.c_str(), S_IRWXU) < -1) {
+		ERROR(errno << ": Could not create directory \"" << path << "\"");
+		return false;
+	}
+	return true;
+}
+
+bool mkDirRecursive(std::string const& path) {
+	std::vector<std::string> dirs = strSplit(path, '/');
+	std::string builtPath = "";
+	for (auto d : dirs) {
+		builtPath += "/" + d;
+		if (!pathExists(builtPath))
+			if (!mkDir(builtPath))
+				return false;
+	}
+	return true;
+}
+
+std::string getFileName(std::string const& path) {
+	if (path.find('/') != path.npos)
+		return path.c_str() + path.find_last_of('/') + 1;
+	else
+		return path;
+}
+
+std::string stripExt(std::string const& path) {
+	auto ppos = path.find_last_of('.');
+	if (ppos != path.npos)
+		return path.substr(0, ppos);
+	else
+		return path;
+}
+
+std::string getFileExt(std::string const& path) {
+	auto ppos = path.find_last_of('.');
+	if (ppos != path.npos)
+		return path.substr(ppos);
+	else
+		return "";
+}
+
+unsigned long getFileTimestamp(std::string const& path) {
+	LOGPREFIX("getFileTimestamp");
+	struct stat fileInfo;
+	if (stat(path.c_str(), &fileInfo) < 0) {
+		ERROR("Could not stat() file \"" << path << "\"");
+		return 0;
+	}
+	return (unsigned long)fileInfo.st_mtim.tv_sec;
+}
+
+bool copyFile(std::string const& source, std::string const& dest) {
+	LOGPREFIX("copyFile");
+	bool ret = true;
+	int fs = open(source.c_str(), O_RDONLY, 0);
+	if (fs < 0) {
+		ERROR(errno << ": Could not open source file \"" << source <<"\"");
+		ret = false;
+	}
+	int fd = open(dest.c_str(), O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd < 0) {
+		ERROR(errno << ": Could not open destination file \"" << dest <<"\"");
+		ret = false;
+	}
+
+	if (ret) {
+		struct stat stat_source;
+		if (fstat(fs, &stat_source) < 0) {
+			ERROR(errno << ": Could not stat() source file \"" << source << "\"");
+			ret = false;
+		}
+		if (ret && sendfile(fd, fs, 0, stat_source.st_size) < 0) {
+			ERROR(errno << ": Could not copy file \n" <<
+					"\"" << source <<"\" -> \"" << dest << "\"");
+			ret = false;
+		}
+	}
+
+	close(fs);
+	close(fd);
+
+	return ret;
+}
+
+bool deleteFile(std::string const& path) {
+	LOGPREFIX("deleteFile");
+	if (unlink(path.c_str()) < 0) {
+		ERROR(errno << ": Could not delete file \"" << path << "\"");
+		return false;
+	}
+	return true;
+}
+
+std::vector<std::string> getFiles(std::string const& baseDir) {
+	LOGPREFIX("getFiles");
+	std::vector<std::string> files;
+    DIR *dp;
+    struct dirent *dirp;
+    if ((dp = opendir(baseDir.c_str())) == NULL) {
+    	ERROR(errno << ": Could not open directory \"" << baseDir << "\"\n");
+    } else {
+        while ((dirp = readdir(dp)) != NULL) {
+            if (dirp->d_name != std::string(".") && dirp->d_name != std::string(".."))
+				files.push_back(baseDir + dirp->d_name);
+        }
+        closedir(dp);
+    }
+    return files;
+}
+
+void applyRecursive(std::string const& baseDir, std::function<void(std::string const& filename)> func) {
+	std::vector<std::string> files = getFiles(baseDir);
+	for (auto f : files) {
+		if (isDir(f))
+			applyRecursive(f, func);
+		else
+			func(f);
+	}
+}
+
+} // namespace
---
+++ src/utils/strManip.cpp
@@ -0,0 +1,78 @@
+/*
+ * strManip.cpp
+ *
+ *  Created on: Nov 13, 2015
+ *      Author: bog
+ */
+
+#include <boglfw/utils/strManip.h>
+
+#include <algorithm>
+#include <locale>
+
+std::vector<std::string> strSplit(std::string const& text, char sep) {
+	return strSplit(text, std::vector<char>{sep});
+}
+
+std::vector<std::string> strSplit(std::string const& text, std::vector<char> const& sep) {
+	std::vector<std::string> tokens;
+	size_t start = 0, end = 0;
+	do {
+		end = text.npos;
+		for (auto c : sep) {
+			auto end1 = text.find(c, start);
+			if (end1 < end)
+				end = end1;
+		}
+		if (end != text.npos) {
+			tokens.push_back(text.substr(start, end - start));
+			start = end + 1;
+		}
+	} while (end != text.npos);
+
+	tokens.push_back(text.substr(start));
+	return tokens;
+}
+
+std::vector<std::string> strSplitPreserveQuotes(std::string const& text, std::vector<char> const& sep) {
+	auto q = strSplit(text, '"');
+	std::vector<std::string> res;
+	for (int i=0; i<q.size(); i++) {
+		if (i%2)	// odd parts are between quotes, we push them as-is
+			res.push_back(q[i]);
+		else {
+			auto qc = strSplit(q[i], sep);	// even parts must be split by usual separators
+			for (auto w : qc)
+				res.push_back(w);	// and pushed word by word
+		}
+	}
+	return res;
+}
+
+std::string strLower(std::string const& str) {
+	std::locale loc("en_US.UTF8");
+	auto &f = std::use_facet<std::ctype<char> >(loc);
+	std::string res(str.c_str());
+	f.tolower(const_cast<char*>(res.c_str()), res.c_str() + res.length());
+	return res;
+}
+
+std::string strUpper(std::string const& str) {
+	std::locale loc("en_US.UTF8");
+	auto &f = std::use_facet<std::ctype<char> >(loc);
+	std::string res(str.c_str());
+	f.toupper(const_cast<char*>(res.c_str()), res.c_str() + res.length());
+	return res;
+}
+
+void replaceAllSubstr(std::string &str, std::string const& what, std::string const& replacement) {
+	size_t pos = 0;
+	while ((pos = str.find(what, pos)) != str.npos) {
+		str.replace(str.begin()+pos, str.begin()+pos+what.length(), replacement);
+		pos += replacement.length();
+	}
+}
+
+unsigned getNumberOfWords(std::string const& str) {
+	return std::count(str.begin(), str.end(), ' ');
+}
